9b33267e69198bd39cf81d4d54c7af73608e8fb4
Mohamed75 <m.boumansour@hotmail.fr>
2015-06-25 14:52:20 +0200

    Initial Commit

---
+++ EmptyLegs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmptyLegs
+//
+//  Created by Mohamed Boumansour on 25/06/15.
+//  Copyright (c) 2015 explovia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
